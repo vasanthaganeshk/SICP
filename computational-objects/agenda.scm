a394ce12d6d76d635d003cce44fadc6fb522cb4e
Vasantha Ganesh K <vasanthaganesh.k@tutamail.com>
2016-07-07 22:15:38 +0530

    Created the table like ADT for agenda that is required for the delay

--- computational-objects/agenda.scm
+++ computational-objects/agenda.scm
@@ -1,21 +1,46 @@
 (define-module (agenda)
-  #:use-module (queue)
-  #:export (make-agenda
-	    current-time
-	    empty-agenda?
-	    add-to-agenda!
-	    first-item
-	    remove-first-item))
-(define (make-time-segment time 
+  #:use-module (queue))
+
+;; Related to each time segment in an agenda
+
+(define (make-segment time queue)
+  (cons time queue))
+
+(define (first segments)
+  (car segments))
+
+(define (rest segments)
+  (cdr segments))
+
+;; Related to agenda
 
 (define (make-agenda)
-  (cons 0 null))
+  (cons 0 `()))
 
 (define (current-time agenda)
   (car agenda))
 
-(define (empty-agenda? agenda)
-  (null? (cdr agenda)))
+(define (segments agenda)
+  (cdr agenda))
 
-(define (add-to-agenda! time proc agenda)
-  (cond ((
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
+
+(define (set-current-time! agenda time)
+  (set-car! agenda time))
+
+(define (pop-first-item! agenda)
+  (let ([first-item (first (segments agenda))])
+    (set-current-time! agenda (car first-item))
+    (set-cdr! (first agenda) (rest (segments agenda)))
+    first-item))
+
+(define (add-to-agenda! time action agenda)
+  (let ([new-seg (make-segment time (insert-queue! (make-queue) action))])
+    (cond ((null? (cdr agenda))
+	   (set-cdr! (car agenda) new-seg))
+	  ((eq? time (caar agenda))
+	   (insert-queue! (cdr (first agenda)) action))
+	  ((< time (cadr agenda))
+	   (set-cdr! (car agenda) new-seg))
+	  (else (add-to-agenda! time action (rest agenda))))))
