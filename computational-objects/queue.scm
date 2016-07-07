3d003743878101e0623585e1e4e17705ba770b96
Vasantha Ganesh K <vasanthaganesh.k@tutamail.com>
2016-07-07 09:50:42 +0530

    queue ADT works

--- computational-objects/queue.scm
+++ computational-objects/queue.scm
@@ -9,9 +9,9 @@
   (cons `() `()))
 
 (define (empty-queue? queue)
-  (null? (front queue)))
+  (null? (car queue)))
 
-(define (insert-queue! obj queue)
+(define (insert-queue! queue obj)
   (let ([elem (cons obj `())])
     (cond ((empty-queue? queue)
 	   (set-car! queue elem)
@@ -19,15 +19,12 @@
 	  (else (set-cdr! (cdr queue) elem)
 		(set-cdr! queue elem)))))
 
-(define (remove-queue! queue)
-  (cond ((eq? (length queue) 1)
-	 (error "ERROR: Cannot remove element from an empty queue."))
-	((eq? (length queue) 2)
-	 (set-car! `())
-	 (set-cdr! `()))
-	(else (set-car! queue (cdr queue)))))
-
 (define (front-queue queue)
   (cond ((empty-queue? queue)
-	 (error "ERROR: No element in the queue"))
-	(else (caar queue))))
+	 (error "No element in the queue"))
+	(caar queue)))
+
+(define (remove-queue! queue)
+  (cond ((empty-queue? queue)
+	 (error "Cannot Remove from an empty queue"))
+	(else (set-car! queue (cdr (car queue))))))
